695b63b6bbde0c362d3baa99e763357a4bb7f27d
rz94 <rafal.zielo94@gmail.com>
2024-03-28 20:35:21 +0100

    Initial commit

---
+++ src/main/java/com/example/aidevs2rz94/AiDevs2rz94Application.java
@@ -0,0 +1,13 @@
+package com.example.aidevs2rz94;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDevs2rz94Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiDevs2rz94Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aidevs2rz94/AiDevs2rz94ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aidevs2rz94;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiDevs2rz94ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
